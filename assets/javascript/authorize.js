5c72f75bc851cdfc18a983c4e43508c0dc31b0d3
santech_montage <santech_montage@mail.ru>
2025-10-04 14:28:11 +0300

    form transition add

--- assets/javascript/authorize.js
+++ assets/javascript/authorize.js
@@ -1,17 +1,13 @@
 function showRegisterForm() {
-    const form = document.getElementById();
-    form.style.display("none");
-    document.querySelector('.container').classList.add('show-register');
+    // Убираем активность с авторизации
+    document.querySelector('.authorize-form').classList.remove('active');
+    // Добавляем активность регистрации
+    document.querySelector('.register-form').classList.add('active');
 }
 
 function showAuthorizeForm() {
-    document.querySelector('.container').classList.remove('show-register');
-}
-
-function TryRegisterCredential() {
-    const text = document.getElementsByClassName("success-message");
-    console.log(text);
-    for (let i = 0; i < text.length; i++) {
-        text[i].style.display = text[i].style.display == "none" ? "block" : "none";
-    }
+    // Убираем активность с регистрации
+    document.querySelector('.register-form').classList.remove('active');
+    // Добавляем активность авторизации
+    document.querySelector('.authorize-form').classList.add('active');
 }
