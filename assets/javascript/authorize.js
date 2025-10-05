6c3f929539b25f23231e433b2fb421c6d62eae85
santech_montage <santech_montage@mail.ru>
2025-10-05 16:43:54 +0300

    authorization using server implemented

--- assets/javascript/authorize.js
+++ assets/javascript/authorize.js
@@ -1,3 +1,5 @@
+/* Обработчики для компонентов */
+
 function showAuthorizeForm() {
     // Скрываем все формы
     document.querySelectorAll('.form').forEach(form => {
@@ -21,16 +23,42 @@ function showForgotPasswordForm() {
     document.querySelector('.recovery-form').classList.add('active');
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const authInputs = document.querySelectorAll('.authorize-form input');
+    const authOutputMessage = document.querySelector('.authorize-form .output-message');
+
+    function hideAuthErrorMessage() {
+        AuthService.hideErrorMessage(authOutputMessage);
+    }
+    
+    authInputs.forEach(input => {
+        //input.addEventListener('mouseenter', hideAuthErrorMessage);
+        input.addEventListener('focus', hideAuthErrorMessage);
+       // input.addEventListener('input', hideAuthErrorMessage);
+    });
+});
+
+/* Сервис авторизации */
 
 class AuthService {
-    static API_BASE_URL = 'http://localhost:5091/api/Credential'; // ← порт 5001!
+    static API_BASE_URL = 'http://localhost:5091/api/Credential';
 
     static async login() {
         const loginInput = document.getElementById('loginAuthorize');
         const passwordInput = document.getElementById('passwordAuthorize');
         
-        const login = loginInput.value;
-        const password = passwordInput.value;
+        const login = loginInput.value.toString();
+        const password = passwordInput.value.toString();
+
+        
+        var outputText = document.querySelector('.output-message');
+        if (login.trim().length === 0) {
+            this.setTextMessage(outputText, true, 'Введите логин');
+            return;
+        } else if (password.trim().length === 0) {
+            this.setTextMessage(outputText, true, 'Введите пароль');
+            return;
+        }
 
         console.log("Sending request to:", `${this.API_BASE_URL}/login`);
 
@@ -50,21 +78,50 @@ class AuthService {
             console.log("Response status:", response.status);
             
             if (!response.ok) {
-                const errorText = await response.text();
-                console.error("Login failed:", response.status, errorText);
+                try {
+                    const errorData = await response.json();
+                    console.error("Ошибка авторизации: ", response.status, errorData);
 
+                    let errorMessage = errorData.message;
+                    this.setTextMessage(outputText, true, errorMessage);
+                } catch (jsonError) {
+                    console.log('Ошибка парсинга message -> string: ', response.status);
+                    this.setTextMessage(outputText, true, 'Внутренняя ошибка. Попробуйте позже');
+                }
                 return;
             }
             
-            if (response.ok) {
-                const data = await response.json();
-                console.log("Login successful:", data);
-            } else {
-                
-            }
+            const data = await response.json();
+
+            this.setTextMessage(outputText, false, 'Авторизация прошла успешно');
+            console.log("Авторизация прошла успешно: ", data);
+
         } catch (error) {
-            console.error("Login error:", error);
-            alert("Login error: " + error.message);
+            console.error("Ошибка авторизации:", error);
+            this.setTextMessage(outputText, true, 'Внутреняя ошибка. Попробуйте позже');
+        }
+    }
+
+    static setTextMessage(label, isError, message, displayStyle = 'block') {
+        label.textContent = message;
+        
+        if (displayStyle === 'block') {
+            label.style.animation = 'slideUp 0.5s ease-in-out';
+            label.style.display = 'block';
+        } else {
+            label.style.animation = 'slideDown 0.5s ease-in-out';
+            label.style.display = 'none';
+        }
+        
+        label.style.color = isError === true ? 'red' : 'green';
+    }
+
+    static hideErrorMessage(label) {
+        if (label && label.style.display === 'block') {
+            label.style.animation = 'slideDown 0.5s ease-out';
+            setTimeout(() => {
+                label.style.display = 'none';
+            }, 450);
         }
     }
 }
