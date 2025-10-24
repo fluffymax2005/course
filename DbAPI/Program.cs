d7ed932620440680d2182c3435d7f1c723a35760
santech_montage <santech_montage@mail.ru>
2025-10-24 15:20:09 +0300

    directives update

--- DbAPI/Program.cs
+++ DbAPI/Program.cs
@@ -5,7 +5,7 @@ using DbAPI.Contexts;
 using DbAPI.Interfaces;
 using DbAPI.Models;
 using DbAPI.Repositories;
-using DbAPI.Repositories.Services;
+using DbAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
